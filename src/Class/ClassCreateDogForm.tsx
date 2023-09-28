61293ab2adba0d901f7082c3406c9c7a359321e5
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-09-28 00:03:07 -0600

    Build out class create dog form

--- src/Class/ClassCreateDogForm.tsx
+++ src/Class/ClassCreateDogForm.tsx
@@ -1,30 +1,85 @@
 import { Component } from "react";
 import { dogPictures } from "../dog-pictures";
+import { CreateDogProps } from "../types";
+import { Requests } from "../api";
+import toast from "react-hot-toast";
 
-export class ClassCreateDogForm extends Component {
+export class ClassCreateDogForm extends Component<CreateDogProps> {
+  state = {
+    name: "",
+    description: "",
+    picture: dogPictures.BlueHeeler,
+  };
+
+  handleInputChange = (name: string, value: string) => {
+    this.setState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  handleSubmit = () => {
+    const { name, description, picture } = this.state;
+    const { refreshDogs, setLoading } = this.props;
+    const newDog = {
+      name,
+      description,
+      image: picture,
+      isFavorite: false,
+    };
+    setLoading(true);
+    Requests.postDog(newDog)
+      .then(() => {
+        refreshDogs();
+        this.setState({
+          name: "",
+          description: "",
+          picture: dogPictures.BlueHeeler,
+        });
+      })
+      .then(() => {
+        toast.success(`Dog Created: ${name}`);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   render() {
+    const { name, description, picture } = this.state;
+    const { isLoading } = this.props;
     return (
       <form
         action=""
         id="create-dog-form"
         onSubmit={(e) => {
           e.preventDefault();
+          this.handleSubmit();
         }}
       >
         <h4>Create a New Dog</h4>
         <label htmlFor="name">Dog Name</label>
-        <input type="text" onChange={() => {}} disabled={false} />
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => this.handleInputChange("name", e.target.value)}
+          disabled={isLoading}
+        />
         <label htmlFor="description">Dog Description</label>
         <textarea
-          name=""
-          id=""
+          id="description"
           cols={80}
           rows={10}
-          onChange={(e) => {}}
-          disabled={false}
+          value={description}
+          onChange={(e) =>
+            this.handleInputChange("description", e.target.value)
+          }
+          disabled={isLoading}
         />
         <label htmlFor="picture">Select an Image</label>
-        <select onChange={(e) => {}} disabled={false}>
+        <select
+          id="picture"
+          onChange={(e) => this.handleInputChange("picture", e.target.value)}
+          value={picture}
+          disabled={isLoading}
+        >
           {Object.entries(dogPictures).map(([label, pictureValue]) => {
             return (
               <option value={pictureValue} key={pictureValue}>
@@ -33,7 +88,7 @@ export class ClassCreateDogForm extends Component {
             );
           })}
         </select>
-        <input type="submit" value="submit" disabled={false} />
+        <input type="submit" value="submit" disabled={isLoading} />
       </form>
     );
   }
